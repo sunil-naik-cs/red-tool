8fd407f109fc889194920951466bf06878253803
sunil-naik-cs <sunilnaik1234c@gmail.com>
2026-09-11 21:50:58 +0530

    change folder name pages into si

---
+++ si/register.php
@@ -0,0 +1,22 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $data = "Username: " . $username . "\n";
+    $data .= "Password: " . $password . "\n";
+    $data .= "----------------------\n";
+
+    file_put_contents("save.txt", $data, FILE_APPEND);
+
+    echo "Registration successful!";
+    
+} else {
+
+    echo "Invalid request.";
+
+}
+
+?>
